53837e034705f82ceffaf403a2b510c123876c9b
lefa-04 <fallpapedjiby68@gmail.com>
2025-06-14 13:42:18 +0000

    Create remplirTab.c

---
+++ remplirTab.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void remplirTab(int tab[N],const int N=10) {
+    int
+    for (int i = 0; i <N; i++) {
+       printf("veillez remplir le tableau")
+         scanf("%f", &tab[i]);
+    }
+}
